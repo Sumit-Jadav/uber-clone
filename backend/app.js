492e596ec072b351cd8ba18035890b94e5b52627
Sumit Jadav <jadavsumit987@gmail.com>
2025-07-20 18:28:15 +0530

    App file

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
 const app = express();
 const connectToDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 connectToDB();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use("/users", userRoutes);
 
