d309098b9f2646529d306c19ca6b45b5c7c1a8a4
Sumit Jadav <jadavsumit987@gmail.com>
2025-07-20 21:56:57 +0530

    captain route added

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,13 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const connectToDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 connectToDB();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 
 module.exports = app;
