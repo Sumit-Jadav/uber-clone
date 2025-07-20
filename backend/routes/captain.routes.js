93a226aa9d6ff6bc79fe904938a1e0c8afde85ce
Sumit Jadav <jadavsumit987@gmail.com>
2025-07-20 21:56:35 +0530

    Registration route added

---
+++ backend/routes/captain.routes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const captainController = require("../controllers/captain.controller");
+const { body } = require("express-validator");
+const captainRouter = express.Router();
+
+captainRouter.post(
+  "/register",
+  [
+    body("fullName.firstName")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+    body("fullName.lastName")
+      .optional()
+      .isLength({ min: 3 })
+      .withMessage("Last name must be at least 3 characters long"),
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+    body("vehicle.color")
+      .isLength({ min: 3 })
+      .withMessage("Color must be at lest 3 characters long"),
+    body("vehicle.plate")
+      .isLength({ min: 3 })
+      .withMessage("Plate number must be at lest 3 characters long"),
+    body("vehicle.capacity")
+      .isInt({ min: 1 })
+      .withMessage("Capacity must be at least 1"),
+    body("vehicle.vehicleType")
+      .isIn(["car", "bike", "auto"])
+      .withMessage("Vehicle type must be car, bike, or auto"),
+  ],
+  captainController.registerCaptain
+);
+
+module.exports = captainRouter;
