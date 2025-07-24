0dd722e1b8f928da440e177b3836437e2b42ccb8
Sumit Jadav <jadavsumit987@gmail.com>
2025-07-24 14:22:56 +0530

    Captain authentication complete

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,3 +1,4 @@
+const blacklistTokenModel = require("../models/blacklistToken.model");
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
 const { validationResult } = require("express-validator");
@@ -18,7 +19,7 @@ module.exports.registerCaptain = async (req, res, next) => {
     firstName: fullName.firstName,
     lastName: fullName.lastName,
     email,
-    password,
+    password: hashPassword,
     color: vehicle.color,
     plate: vehicle.plate,
     capacity: vehicle.capacity,
@@ -29,4 +30,46 @@ module.exports.registerCaptain = async (req, res, next) => {
     token,
     captain,
   });
+};
+
+module.exports.loginCaptain = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+  const captain = await captainModel.findOne({ email }).select("+password");
+  if (!captain) {
+    return res.status(400).json({ message: "Invalid email or password!" });
+  }
+  const isMatch = await captain.comparePassword(password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid email or password!" });
+  }
+  const token = captain.generateAuthToken();
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+  return res.status(200).json({
+    token,
+    captain,
+  });
+};
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+  return res.status(200).json({
+    captain: req.captain,
+  });
+};
+
+module.exports.logoutCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(400).json({ message: "No token privided!" });
+  }
+  await blacklistTokenModel.create({ token });
+  res.clearCookie("token");
+  return res.status(200).json({ message: "Logged out successfully!" });
 };
