5f60c91389a7f25cd632bf3b37a92f363cf133e3
Sumit Jadav <jadavsumit987@gmail.com>
2025-07-25 17:51:58 +0530

    User signup page

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const UserSignup = () => {
-  return <div>UserSignup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userData, setUserData] = useState({});
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      fullName: {
+        firstName,
+        lastName,
+      },
+      email: email,
+      password: password,
+    });
+    console.log(userData);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="p-7 flex flex-col justify-between h-screen">
+      <div>
+        <img className="w-16 mb-10" src="Uber.png" alt="" />
+
+        <form action="" onSubmit={(e) => submitHandler(e)}>
+          <h3 className="text-lg font-medium mb-2">What's your name</h3>
+          <div className="flex gap-4 ">
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              name="first_name"
+              className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
+              id="first_user_name"
+              placeholder="First Name"
+              required
+            />
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              name="last_name"
+              className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
+              id="last_user_name"
+              placeholder="Last Name"
+            />
+          </div>
+          <h3 className="text-lg font-medium mb-2">What's your email</h3>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="user_email"
+            className="bg-[#eeee] mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base "
+            id="user_email"
+            placeholder="email@example.com"
+            required
+          />
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="user_password"
+            className="bg-[#eeee] mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base "
+            placeholder="password"
+            id="user_password"
+            required
+          />
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base ">
+            Register
+          </button>
+        </form>
+        <p className="text-center">
+          Already have Account?{" "}
+          <Link to="/login" className="text-blue-600  ">
+            Login Here
+          </Link>
+        </p>
+      </div>
+      <div>
+        <p className="text-[10px] leading-tight">
+          This site is protected by reCAPTCHA and the{" "}
+          <span className="underline">Google Privacy Policy</span> and{" "}
+          <span className="underline">Terms of Service apply</span>.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default UserSignup;
