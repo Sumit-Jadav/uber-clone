83047ecf838818bb1c21d7387a896d74a326db48
Sumit Jadav <jadavsumit987@gmail.com>
2025-07-25 19:04:07 +0530

    User Context

---
+++ frontend/src/contex/UserContex.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export const UserContext = React.createContext();
+
+const UserContex = ({ children }) => {
+  const [user, setUser] = useState({
+    email: "",
+    fullName: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  return (
+    <div>
+      <UserContext.Provider value={user}>{children}</UserContext.Provider>
+    </div>
+  );
+};
+
+export default UserContex;
