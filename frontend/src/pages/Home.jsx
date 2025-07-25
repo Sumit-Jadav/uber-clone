0f8081716fc09ba5d50fe761607d3e1d225e360c
Sumit Jadav <jadavsumit987@gmail.com>
2025-07-25 19:03:04 +0530

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <div className="bg-cover bg-center bg-[url('traffic.avif')] h-screen pt-8  w-full  flex  justify-between flex-col">
+      <div className="bg-contain  bg-top  bg-[url('traffic.avif')] h-screen pt-8  w-full  flex  justify-between flex-col">
         <img className="w-16 ml-9" src="Uber.png" alt="" />
         <div className="bg-white w-full pb-7 py-4 px-4">
-          <h2 className="text-2xl font-bold">Get Started with Uber</h2>
+          <h2 className="text-[30px] font-bold">Get Started with Uber</h2>
           <Link
             to="/login"
-            className="flex items-center justify-center bg-black w-full text-white py-3 rounded mt-5"
+            className="flex items-center justify-center bg-black w-full text-white py-3 rounded-lg mt-5"
           >
             Continue
           </Link>
