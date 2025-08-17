acb8261901bca7a7037f78ae1817f54e89faab28
Sumit Jadav <jadavsumit987@gmail.com>
2025-08-17 20:32:08 +0530

    Captain register page backend integration complete

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,27 +1,59 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { CaptainContext } from "../contex/CaptainContex";
 const CaptainSignup = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [captainData, setCaptainData] = useState({});
-  const submitHandler = (e) => {
+  const [plate, setPlate] = useState("");
+  const [color, setColor] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [vehicleType, setVehicleType] = useState("");
+  // const [captainData, setCaptainData] = useState({});
+
+  const { captain, setCaptain } = React.useContext(CaptainContext);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setCaptainData({
+    const captainData = {
       fullName: {
         firstName,
         lastName,
       },
       email: email,
       password: password,
-    });
+      vehicle: {
+        color,
+        plate,
+        capacity,
+        vehicleType,
+      },
+    };
     console.log(captainData);
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captains/register`,
+      captainData
+    );
+
+    if (response.status === 201) {
+      const data = response.data;
+      setCaptain(data.captain);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-login");
+    }
+
     setFirstName("");
     setLastName("");
     setEmail("");
     setPassword("");
+    setPlate("");
+    setColor("");
+    setCapacity(1);
+    setVehicleType("");
   };
   return (
     <div className="p-7 flex flex-col justify-between h-screen">
@@ -36,7 +68,7 @@ const CaptainSignup = () => {
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
               name="first_name"
-              className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
+              className="bg-[#eeee] mb-3 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
               id="first_user_name"
               placeholder="First Name"
               required
@@ -46,7 +78,7 @@ const CaptainSignup = () => {
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
               name="last_name"
-              className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
+              className="bg-[#eeee] mb-3 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
               id="last_user_name"
               placeholder="Last Name"
             />
@@ -57,7 +89,7 @@ const CaptainSignup = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             name="user_email"
-            className="bg-[#eeee] mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base "
+            className="bg-[#eeee] mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base "
             id="user_email"
             placeholder="email@example.com"
             required
@@ -68,11 +100,60 @@ const CaptainSignup = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             name="user_password"
-            className="bg-[#eeee] mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base "
+            className="bg-[#eeee] mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base "
             placeholder="password"
             id="user_password"
             required
           />
+          <h3 className="text-lg font-medium mb-2">Details Of Vehicle</h3>
+          <div className="flex flex-col gap-0">
+            <div className="flex gap-4 mb-0">
+              <input
+                type="text"
+                value={plate}
+                onChange={(e) => setPlate(e.target.value)}
+                name="vehicle_plate"
+                className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
+                placeholder="Plate Number"
+                id="vehicle_plate"
+                required
+              />
+              <input
+                type="text"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+                name="vhicle_color"
+                className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
+                placeholder="Vehicle Color"
+                id="vhicle_color"
+                required
+              />
+            </div>
+            <div className="flex gap-4 mb-0">
+              <input
+                type="number"
+                value={capacity}
+                onChange={(e) => setCapacity(e.target.value)}
+                name="vehicle_capacity"
+                className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base "
+                placeholder="Capacity"
+                id="vehicle_capacity"
+                required
+              />
+              <select
+                value={vehicleType}
+                className="bg-[#eeee] mb-7 rounded px-4 py-2  w-1/2 text-lg placeholder:text-base"
+                onChange={(e) => setVehicleType(e.target.value)}
+              >
+                <option value="" disabled>
+                  Select
+                </option>
+                <option value="car">Car</option>
+                <option value="bike">Bike</option>
+                <option value="auto">Auto</option>
+              </select>
+            </div>
+          </div>
           <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base ">
             Register
           </button>
